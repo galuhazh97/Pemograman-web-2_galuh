d638c6ef5b203242da26abeec17f44f0dd6f6678
Galuh Azhary Putri <114564661+galuhazh97@users.noreply.github.com>
2023-03-13 12:47:07 +0700

    reupload tugas praktik

---
+++ praktikum3/praktik/proses_registrasi.php
@@ -0,0 +1,23 @@
+<?php
+
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital",
+];
+
+$skills = [
+    "HTML" => 10,
+    "CSS" => 10,
+    "JavaScript" => 20,
+    "PHP" => 30,
+    "Python" => 30,
+]
+?>
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
---
+++ praktikum3/tugas/proses_registrasi.php
@@ -0,0 +1,21 @@
+<?php
+
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+
+$program_studi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital",
+];
+
+$skills = [
+    "HTML" => 10,
+    "CSS" => 10,
+    "JavaScript" => 20,
+    "PHP" => 30,
+    "Python" => 30,
+    "RWD" => 50,
+]
+
+
+?>
